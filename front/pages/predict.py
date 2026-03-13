db472755a08f27bdf1a92792112b3e6f5e347e33
asticotcoco <jccharbo1@gmail.com>
2026-03-13 17:22:31 +0000

    modifiestestfix predict

--- front/pages/predict.py
+++ front/pages/predict.py
@@ -1,30 +1,19 @@
-import os
-import time
-
 import requests
 import streamlit as st
 
-from diamonds.data import load_data
-
-
-@st.cache_data
-def load_diamonds_data():
-    with st.spinner("Loading data..."):
-        time.sleep(2)  # Simulate loading time
-        return load_data()
-
-df_diamonds = load_diamonds_data()
-
-API_HOST = os.environ.get("API_HOST", "http://localhost:8888").rstrip("/")
-
+# api url
+API_HOST = "https://diamonds-861302064365.europe-west1.run.app"
+CUT_OPTIONS = ["Fair", "Good", "Very Good", "Premium", "Ideal"]
+COLOR_OPTIONS = ["D", "E", "F", "G", "H", "I", "J"]
+CLARITY_OPTIONS = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
 
 st.markdown("## Predict Price")
 
 with st.form("predict_form"):
     carat = st.slider("Carat", min_value=0.2, max_value=5.0, step=0.1)
-    cut = st.selectbox("Cut", options=df_diamonds["cut"].unique())
-    color = st.selectbox("Color", options=df_diamonds["color"].unique())
-    clarity = st.selectbox("Clarity", options=df_diamonds["clarity"].unique())
+    cut = st.selectbox("Cut", CUT_OPTIONS)
+    color = st.selectbox("Color", COLOR_OPTIONS)
+    clarity = st.selectbox("Clarity", CLARITY_OPTIONS)
     depth = st.slider("Depth", min_value=50.0, max_value=70.0, step=0.1)
     table = st.slider("Table", min_value=50.0, max_value=70.0, step=0.1)
     x = st.number_input("X (length in mm)", min_value=0.0, max_value=10.0, step=0.1)
